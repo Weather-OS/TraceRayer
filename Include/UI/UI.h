7eff196a77cba9999b943b9d1fc5a93eaa6efd23
tpapata1386 <tpapata1386@gmail.com>
2025-12-26 16:29:18 +0330

    Add a UI.h file to unify all UI headers.

---
+++ Include/UI/UI.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2025 Weather
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef TRACERAYER_UI_H
+#define TRACERAYER_UI_H
+
+#include <UI/GTK/GTK.h>
+#include <UI/GTK/GTKBox.h>
+#include <UI/GTK/GTKDrawingArea.h>
+#include <UI/GTK/GTKLabel.h>
+#include <UI/GTK/GTKOverlay.h>
+#include <UI/GTK/GTKPicture.h>
+#include <UI/GTK/GTKSpinner.h>
+#include <UI/GTK/GTKWidget.h>
+#include <UI/GTK/GTKWindow.h>
+#include <UI/GTK/GTKWindowHandle.h>
+
+#endif
